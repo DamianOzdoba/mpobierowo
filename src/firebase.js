471093dae6722ae6f1a3233b7a9f4ac2254b22f6
damian <damian@iMac.local>
2022-06-11 19:12:42 +0200

    rwd update

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnB14TG0-tFQMgaeFYYzQ4fP-zSEn8e0s",
-    authDomain: "mpobierowo-4e001.firebaseapp.com",
-    projectId: "mpobierowo-4e001",
-    storageBucket: "mpobierowo-4e001.appspot.com",
-    messagingSenderId: "43018229989",
-    appId: "1:43018229989:web:90347ba9d9d17594815c0c"
+    apiKey: "AIzaSyB4-4HItmCeVAhZJXAtuysPSQPWFLSWuro",
+    authDomain: "mpobierowo-27c98.firebaseapp.com",
+    projectId: "mpobierowo-27c98",
+    storageBucket: "mpobierowo-27c98.appspot.com",
+    messagingSenderId: "152638670275",
+    appId: "1:152638670275:web:1dc05f3aea7b67a903e976"
 };
 
 firebase.initializeApp(firebaseConfig);
